af61b0b1dcbe5dd79a6a1206fdf45e66056af88e
lixy1223 <2392073222@qq.com>
2021-10-10 17:13:38 +0800

    test

--- ext1/exc4/main.go
+++ ext1/exc4/main.go
@@ -4,5 +4,16 @@
 package main
 import "fmt"
 func main(){
-
+	var score int
+	fmt.Println("请输入分数：")
+	fmt.Scanln(&score)
+	if score >= 60{
+		fmt.Println("及格")
+	}else{//else不能换行
+		fmt.Println("不及格")
+	}
+	// if score < 60 {
+	// 	fmt.Println("不及格")
+	// }
+	
 }
